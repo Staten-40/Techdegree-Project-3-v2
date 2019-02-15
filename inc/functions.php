59ad46123c9460ed5f2fcaf3ac55b2dd514131af
ShaSta_57 <44626281+Staten-40@users.noreply.github.com>
2019-02-15 05:03:25 -0800

    Error resolved, now for edit entry

--- inc/functions.php
+++ inc/functions.php
@@ -16,10 +16,14 @@
  
 }
 //Add new entry: Created function to include all table elements to new entry
-function add_that_entry($title, $date, $time_spent, $learned, $resources) {
+function add_that_entry($title, $date, $time_spent, $learned, $resources, $entry_id=null) {
     include ('connections.php');
+    if($entry_id) {
+        $sql = 'UPDATE entries SET title=?, date=?, time_spent=?, learned=?, resources=?';
+    } else {
    
     $sql = 'INSERT INTO entries(title, date, time_spent, learned, resources) VALUES(?,?,?,?,?)';
+    }
     try {
 $results = $db->prepare($sql);
 $results->bindValue(1, $title, PDO::PARAM_STR);
@@ -27,6 +31,9 @@ $results->bindValue(2, $date, PDO::PARAM_STR);
 $results->bindValue(3, $time_spent, PDO::PARAM_STR);
 $results->bindValue(4, $learned, PDO::PARAM_STR);
 $results->bindValue(5, $resources, PDO::PARAM_STR);
+        if($entry_id) {
+            $results->bindValue(6, $entry_id, PDO::PARAM_NUMBER_INT);
+         }
 $results->execute();
 } catch(Exception $e) {  
         echo "Error: " . $e->getMessage() . "<br />";
@@ -48,38 +55,5 @@ try {
 $entry = $results->fetch(PDO::FETCH_ASSOC);
 return $entry;
 }
-
-//Edit entry: Created function to edit existing blog entries
-
-function edit_that_entry($title, $date, $time_spent, $learned, $resources) {
-
-    function edit_that_entry($title, $date, $time_spent, $learned, $resources) {
-
-        if(SERVER['REQUEST METHOD'] == 'POST') {
-    
-        $sql = 'UPDATE entries SET title=?, date=?, time_spent=?, learned=?, resources=? WHERE entry_id=?';
-    
-        $entry_id = trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_NUMBER_INT)); 
-        $title = trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING)); 
-        $date = trim(filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING));
-        $time_spent = trim(filter_input(INPUT_POST, 'timeSpent', FILTER_SANITIZE_STRING));
-        $learned = trim(filter_input(INPUT_POST, 'whatILearned', FILTER_SANITIZE_STRING));
-        $resources = trim(filter_input(INPUT_POST, 'ResourcesToRemember', FILTER_SANITIZE_STRING));
-        
-    
-        if (empty($title) || empty($date)) {
-            $error_msg = "PSST!  Hey, you at least needs the title and date!";
-     
-        } else {
-            if(edit_that_entry($title, $date, $time_spent, $learned, $resources, $entry_id)) {
-                header('Location: detail.php');
-                exit; 
-     
-           } else {
-                $error_msg = "Bummer.  Couldn't edit that one.";
-            }       
-            
-        }
-    }
-}
-     ?>   
+?>
+ 
